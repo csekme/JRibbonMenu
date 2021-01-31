b4c5d2f297b3f3ed23a1b9e2913c342e433b1354
Pconti31 <p_conti@hotmail.com>
2021-01-31 13:00:29 -0500

    fixed for issues 3 Can't get tab selection to work

--- src/main/java/hu/csekme/RibbonMenu/RibbonBar.java
+++ src/main/java/hu/csekme/RibbonMenu/RibbonBar.java
@@ -176,11 +176,11 @@ public class RibbonBar extends JComponent {
 	void toggle() {
 		if (minimized) {
 			setMinimumSize(new Dimension(100, ribbonTabHeight + shadowHeight));
-			setPreferredSize(new Dimension(100, ribbonTabHeight + shadowHeight));
+			setPreferredSize(new Dimension(getWidth(), ribbonTabHeight + shadowHeight));
 			setSize(new Dimension(getWidth(), ribbonTabHeight + shadowHeight));
 		} else {
 			setMinimumSize(new Dimension(0, ribbonHeight));
-			setPreferredSize(new Dimension(100, ribbonHeight));
+			setPreferredSize(new Dimension(getWidth(), ribbonHeight));
 			setSize(new Dimension(getWidth(), ribbonHeight));
 		}
 		getParent().revalidate();
@@ -699,8 +699,8 @@ public class RibbonBar extends JComponent {
 					for (int i = 0; i < TABS.size(); i++) {
 						TABS.get(i).setSelected(TABS.get(i).inBounds(e.getPoint(), TABS.get(i).getToken()));
 					}
-					minimized = false;
-					toggle();
+          minimized = false;
+  			  toggle();
 				}  
 			}
 			if (e.getPoint().y > ribbonTabHeight) {
