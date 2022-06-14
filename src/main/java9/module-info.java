a5c9116dfd1a50396c2d47e9c226e95fd4f94807
Pconti31 <p_conti@hotmail.com>
2022-06-14 14:30:58 -0400

    update flatlaf module name to 2.3 version

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -3,6 +3,8 @@
 module JRibbonMenu {
   requires transitive java.desktop;
   exports com.pckcs.RibbonMenu;
+  requires com.formdev.flatlaf;
+  requires com.formdev.flatlaf.intellijthemes;
   exports dist;
   exports images;
   opens dist;
