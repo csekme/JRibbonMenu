11ff62a98de285764f88f670575bde009ea146e6
Pconti31 <p_conti@hotmail.com>
2022-06-15 09:02:39 -0400

    fixes for module using SVG routines

--- src/main/java9/module-info.java
+++ src/main/java9/module-info.java
@@ -3,10 +3,13 @@
 module JRibbonMenu {
   requires transitive java.desktop;
   exports com.pckcs.RibbonMenu;
+  requires com.kitfox.svg; 
   requires com.formdev.flatlaf;
   requires com.formdev.flatlaf.intellijthemes;
+  requires com.formdev.flatlaf.extras;
   exports dist;
   exports images;
   opens dist;
   opens images;
+  opens images.svg;
 }
